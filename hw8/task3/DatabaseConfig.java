039281ba8b7f89ef1456887ae6164271f0b83cd3
Adelante <97695810+MrPesochek@users.noreply.github.com>
2025-04-07 20:08:52 +0300

    Create DatabaseConfig.java

---
+++ hw8/task3/DatabaseConfig.java
@@ -0,0 +1,24 @@
+package com.example.books;
+
+import javax.sql.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+public class DatabaseConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.sqlite.JDBC");
+        dataSource.setUrl("jdbc:sqlite:books.db");
+        return dataSource;
+    }
+    
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+}
