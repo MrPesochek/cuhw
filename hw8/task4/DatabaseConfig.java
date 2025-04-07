c131c5d5f69ff3a431a9ed85ed5b620d1f7d6750
Adelante <97695810+MrPesochek@users.noreply.github.com>
2025-04-07 20:10:58 +0300

    Create DatabaseConfig.java

---
+++ hw8/task4/DatabaseConfig.java
@@ -0,0 +1,26 @@
+package com.example.books;
+
+import javax.sql.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+public class DatabaseConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/cudb");
+        dataSource.setUsername("admin");
+        dataSource.setPassword("admin");
+        return dataSource;
+    }
+    
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+}
